bca3d6d47fd399e66b2d8ea611c4a42c3925d11c
Rizal Muttaqin <kotabandungpcnu@gmail.com>
2026-05-16 23:17:16 +0700

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -76,7 +76,7 @@ export default function LoginPage() {
             </motion.div>
             <h1 className="text-2xl font-bold text-white mb-1">SAFIRA</h1>
             <p className="text-primary-200 text-sm">
-              {orgData?.tingkatanOrg namaOrg daerahOrg || 'Smart Fatayat untuk Informasi Risalah dan Administrasi'}
+              {orgData?.tingkatanOrg orgData?.namaOrg orgData?.daerahOrg || 'Smart Fatayat untuk Informasi Risalah dan Administrasi'}
             </p>
           </div>
 
